551e5befeec43b5b32f5df89fc3dbb63066b825c
Aleksandr <50_Sanya@mail.ru>
2021-03-25 21:35:54 +0700

    Commit 2 - Adapter почему то попал в фвйл gitignore

---
+++ app/src/main/java/com/example/currencyconverter/controller/CurrencyAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.currencyconverter.controller
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.currencyconverter.R
+import com.example.currencyconverter.view.CurrencySelection
+
+class CurrencyAdapter(
+    private val value: ArrayList<ArrayList<String>>?,
+) : RecyclerView.Adapter<CurrencyHolder>() {
+
+    private var selectedCurrencyIndex1: Int? = 10
+    private var selectedCurrencyIndex2: Int? = 11
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyHolder {
+        val context = parent.context
+        val inflater =  LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.currency, parent, false)
+
+        return CurrencyHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CurrencyHolder, position: Int) {
+        val currency = value!![position]
+
+        holder.bind(currency, position, selectedCurrencyIndex1, selectedCurrencyIndex2)
+    }
+
+    override fun getItemCount() = value!!.size
+
+    fun setIntent(intent: Intent?) {
+        selectedCurrencyIndex1 = intent?.getIntExtra("selected1", 10)
+        selectedCurrencyIndex2 = intent?.getIntExtra("selected2", 11)
+    }
+}
